33991a2d44ff9db7b0e3124b310801d7ec25c572
Ronaldo Miral <ronaldomiralp@gmail.com>
2022-10-20 20:31:15 +0100

    Making the conditions to verify wether is or not a prime number

--- NumeroPrimo.java
+++ NumeroPrimo.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class NumeroPrimo {
     public static void main(String[] args) {
-        for(int inteiro = 1; inteiro <= 31; inteiro++) {
-            System.out.println(inteiro);
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int numero = userInput.nextInt();
+        int contador = 0;
+        int primo = 0;
+
+        for(int inteiro = 1; inteiro <= numero; inteiro++) {
+            primo = numero % inteiro;
+
+            if(primo == 0) {
+                contador = contador + 1;
+            }
+        }
+
+        if(contador == 2) {
+            System.out.println("O numero " + numero + " e primo");
+        } else {
+            System.out.println("O numero " + numero + " nao e primo");
+
         }
     }
 }
